0296c11b9077e0b203ce45c6d919b74734752970
Lucas Silva <lucasfsilva2310@gmail.com>
2024-07-15 11:21:30 -0300

    adding h2 run time database and jdbc api to work with in-memory database

--- src/main/java/dev/lucas/runnerz/run/RunController.java
+++ src/main/java/dev/lucas/runnerz/run/RunController.java
@@ -58,7 +58,7 @@ public class RunController {
     // Update
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    void update(@RequestBody Run run, @PathVariable Integer id) {
+    void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
         runRepository.update(run, id);
     }
 
